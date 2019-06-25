f0e27112af368d617e020934605d169b846840a0
Peter Bengtsson <mail@peterbe.com>
2019-06-25 14:57:48 -0400

    prettier

--- bin/update_version.js
+++ bin/update_version.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const spawn = require('child_process').spawnSync;
+const fs = require("fs");
+const spawn = require("child_process").spawnSync;
 
-const package = require('../package.json');
+const package = require("../package.json");
 const name = package.name;
 
 const spawnStr = (cmd, ...args) => {
   const spawned = spawn(cmd, ...args);
   return spawned.stdout.toString().trim();
 };
-const version = spawnStr('git', ['describe', '--always', '--tag']);
+const version = spawnStr("git", ["describe", "--always", "--tag"]);
 
-const logRaw = spawnStr('git', ['log', "--pretty=format:'%H--%cI'", '-n', '1']);
+const logRaw = spawnStr("git", ["log", "--pretty=format:'%H--%cI'", "-n", "1"]);
 
 const log = logRaw.slice(1, logRaw.length - 1);
-const commit = log.split('--')[0];
-const date = log.split('--')[1];
+const commit = log.split("--")[0];
+const date = log.split("--")[1];
 
 console.log(
   JSON.stringify(
