10edfe741a5970bad030dd861f209727bf034565
Peter Bengtsson <mail@peterbe.com>
2019-10-23 12:59:11 -0400

    new stats

--- src/Stats.js
+++ src/Stats.js
@@ -1,15 +1,15 @@
 /** This page is auto-generated from inject-stats.js
- * Date: Thu Oct 03 2019 12:54:54 GMT-0400 (Eastern Daylight Time)
+ * Date: Wed Oct 23 2019 12:58:56 GMT-0400 (Eastern Daylight Time)
  */
 
 const Stats = {
-  "date": 1570121694561,
+  "date": 1571849936817,
   "kuma": {
     "en-US": 18256,
     "all": 64262
   },
   "stumptown": {
-    "en-US": 53,
+    "en-US": 78,
     "all": null
   }
 };
