5382d3f64625ea8146fb824d502f4ede6ecf96fa
Peter Bengtsson <mail@peterbe.com>
2019-10-25 10:06:45 -0400

    cutestrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Bar, Line } from "react-roughviz";
-import "./light.css";
+// import "./light.css";
+import "cutestrap/dist/css/cutestrap.min.css";
+import "./Cutestrap-customizations.css";
 import "./App.css";
 
 import { Stats } from "./Stats";
@@ -11,65 +13,84 @@ export const isServer =
 function App() {
   return (
     <div>
-      <h1>Are We Stumptowned Yet?</h1>
-      <h2>No :(</h2>
+      <header className="header">
+        <h1>Are We Stumptowned Yet?</h1>
+      </header>
 
-      <h3>What's this?</h3>
-      <p>
-        Stumptown is a Mozilla MDN project to rewrite existing documentation
-        about web development, from a{" "}
-        <a href="https://developer.mozilla.org">Wiki</a>, stored in a central
-        relational database, to{" "}
-        <b>strutured content (e.g. Markdown files) in a Git repo</b>.
-      </p>
-      <p>Stumptown consists of two parts:</p>
-      <ul>
-        <li>
-          <a href="https://github.com/mdn/stumptown-content">
-            <b>github.com/mdn/stumptown-content</b>
-          </a>{" "}
-          - the actual content (plus some scripting)
-        </li>
-        <li>
-          <a href="https://github.com/mdn/stumptown-renderer">
-            <b>github.com/mdn/stumptown-renderer</b>
-          </a>{" "}
-          - React components for turning content into HTML web pages
-        </li>
-      </ul>
-      <p>
-        This site is about attempting to measure how many ready pages there are
-        in <b>github.com/mdn/stumptown-content</b> and contrast that with the
-        number of pages in <b>developer.mozilla.org</b>.
-      </p>
+      <main className="main">
+        <h2>
+          No{" "}
+          <span role="img" aria-label="sad">
+            ☹️
+          </span>
+        </h2>
 
-      <h3>Current Status</h3>
-      <dl>
-        <dt>
-          Kuma (<code>en-US</code>):
-        </dt>
-        <dd className="number">{Stats.kuma["en-US"].toLocaleString()}</dd>
+        <h3>What's this?</h3>
+        <p>
+          Stumptown is a Mozilla MDN project to rewrite existing documentation
+          about web development, from a{" "}
+          <a href="https://developer.mozilla.org">Wiki</a>, stored in a central
+          relational database, to{" "}
+          <b>strutured content (e.g. Markdown files) in a Git repo</b>.
+        </p>
+        <p>Stumptown consists of two parts:</p>
+        <ul>
+          <li>
+            <a href="https://github.com/mdn/stumptown-content">
+              <b>github.com/mdn/stumptown-content</b>
+            </a>{" "}
+            - the actual content (plus some scripting)
+          </li>
+          <li>
+            <a href="https://github.com/mdn/stumptown-renderer">
+              <b>github.com/mdn/stumptown-renderer</b>
+            </a>{" "}
+            - React components for turning content into HTML web pages
+          </li>
+        </ul>
+        <p>
+          This site is about attempting to measure how many ready pages there
+          are in <b>github.com/mdn/stumptown-content</b> and contrast that with
+          the number of pages in <b>developer.mozilla.org</b>.
+        </p>
 
-        <dt>
-          Stumptown (<code>en-US</code>):
-        </dt>
-        <dd className="number">{Stats.stumptown["en-US"].toLocaleString()}</dd>
-        <dd>
-          Approximately{" "}
-          {(
-            (100 * Stats.stumptown["en-US"]) /
-            (Stats.stumptown["en-US"] + Stats.kuma["en-US"])
-          ).toFixed(1)}
-          % of Kuma
-        </dd>
-      </dl>
-      <p>
-        <small>As of {prettyTimestamp(Stats.date)}</small>
-      </p>
-      <div>
-        <StatsChart />
-        <HistoryChart />
-      </div>
+        <h3>Current Status</h3>
+        <div>
+          <h5>
+            Kuma (<code>en-US</code>):
+          </h5>
+          <p className="number">{Stats.kuma["en-US"].toLocaleString()}</p>
+
+          <h5>
+            Stumptown (<code>en-US</code>):
+          </h5>
+          <p className="number">{Stats.stumptown["en-US"].toLocaleString()}</p>
+          <p>
+            Approximately{" "}
+            {(
+              (100 * Stats.stumptown["en-US"]) /
+              (Stats.stumptown["en-US"] + Stats.kuma["en-US"])
+            ).toFixed(1)}
+            % of Kuma
+          </p>
+        </div>
+        <p>
+          <small>As of {prettyTimestamp(Stats.date)}</small>
+        </p>
+        <div>
+          <StatsChart />
+          <HistoryChart />
+        </div>
+      </main>
+      <footer className="footer">
+        <section className="grid">
+          <p className="ta-center">
+            <a href="https://github.com/peterbe/arewestumptownedyet/">
+              github.com/peterbe/arewestumptownedyet
+            </a>
+          </p>
+        </section>
+      </footer>
     </div>
   );
 }
